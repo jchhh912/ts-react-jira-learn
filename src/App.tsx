fd5648f35e0b7db03871026670ce4f020a3b6286
jchhh912 <jchhh912@163.com>
2021-12-08 23:34:41 +0800

    fix:添加登录表单

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
-import { ProjectListScreerns } from "screens/project-list";
-import { TsReactTese } from "utils/try-use-array";
+// import { ProjectListScreerns } from "screens/project-list";
+// import { TsReactTese } from "utils/try-use-array";
+import { LoginScreen } from "screens/login";
 
 function App() {
   return (
     <div className="App">
-      <TsReactTese />
-      <ProjectListScreerns />
+      {/* <TsReactTese />
+      <ProjectListScreerns /> */}
+      <LoginScreen />
     </div>
   );
 }
