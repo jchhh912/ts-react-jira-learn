fc35cea515c3a2d05ac525ad361f655175fa1774
jchhh912 <jchhh912@163.com>
2022-01-03 19:52:15 +0800

    style:用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,6 +2,7 @@ import { useAuth } from 'context/auth-context'
 import { ProjectListScreen } from 'screens/project-list'
 import React from 'react'
 import styled from '@emotion/styled'
+import { Row } from 'components/lib'
 /**
  * grid和flex各自的应用场景
  * 1.要考虑是一维布局还是二维布局
@@ -20,59 +21,35 @@ export const AuthenticaedApp = () => {
   const { logout } = useAuth()
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
-          <h3>Logo</h3>
-          <h3>项目</h3>
-          <h3>用户</h3>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
         </HeaderLeft>
         <HeaderRight>
           <button onClick={logout}>登出</button>
         </HeaderRight>
       </Header>
-      <Nav>nav</Nav>
       <Main>
         <ProjectListScreen />
       </Main>
-      <Aside>aside</Aside>
-      <Footer>footer</Footer>
     </Container>
   )
 }
 //1fr 等于自适应 剩下来的都属于它
 const Container = styled.div`
   display: grid;
-  grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas:
-    'header header header'
-    'nav main aside'
-    'footer footer footer';
+  grid-template-rows: 6rem 1fr;
   height: 100vh;
 `
-
 //grid-area 用来给grid子元素起名字
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`
-const HeaderLeft = styled.div`
+const Header = styled(Row)``
+
+const HeaderLeft = styled(Row)`
   display: flex;
   align-items: center;
 `
 const HeaderRight = styled.div``
-const Main = styled.main`
-  grid-area: main;
-`
-const Nav = styled.nav`
-  grid-area: nav;
-`
-const Aside = styled.aside`
-  grid-area: aside;
-`
-const Footer = styled.footer`
-  grid-area: footer;
-`
+
+const Main = styled.main``
