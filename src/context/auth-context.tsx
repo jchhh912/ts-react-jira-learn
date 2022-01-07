c5c5ef5b2c270c8b01a798faeb13238c0da1f845
jchhh912 <jchhh912@163.com>
2022-01-07 23:10:44 +0800

    feat:处理me接口loading和error

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,8 +1,10 @@
-import React, { ReactNode, useState } from 'react'
+import React, { ReactNode } from 'react'
 import * as auth from 'auth-provider'
 import { User } from 'screens/project-list/search-panel'
 import { http } from 'utils/http'
 import { useMount } from 'utils'
+import { useAsync } from 'utils/use-async'
+import { FullPageErrorFallback, FullPageLoading } from 'components/lib'
 
 //定义一个接受类
 interface AuthForm {
@@ -35,7 +37,15 @@ AuthContext.displayName = 'AuthContext'
 //主要的逻辑处理
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   //指定泛型 或者 null
-  const [user, setUser] = useState<User | null>(null)
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser
+  } = useAsync<User | null>()
   //可以转变以下写法 const login = (from: AuthForm) => auth.login(from).then(user=>setUser(user))
   const login = (from: AuthForm) => auth.login(from).then(setUser)
   const register = (from: AuthForm) => auth.register(from).then(setUser)
@@ -43,8 +53,14 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const logout = () => auth.logout().then(() => setUser(null))
   //页面加载时调用初始化
   useMount(() => {
-    bootstapUser().then(setUser)
+    run(bootstapUser())
   })
+  if (isIdle || isLoading) {
+    return <FullPageLoading />
+  }
+  if (isError) {
+    return <FullPageErrorFallback error={error} />
+  }
   return (
     <AuthContext.Provider
       children={children}
